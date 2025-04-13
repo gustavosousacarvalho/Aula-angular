8753272d63f98877fee15118ed3206330ba76c26
Gustavo Carvalho <gc49862@gamail.com>
2025-04-13 16:18:24 -0300

    Curso Angular; Inicio da atividade

---
+++ curso-angular-lista-de-tarefas/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: "<router-outlet />",
+})
+export class AppComponent {}
